19e0511b52e5f4a91ab5277468583aa15746fd80
Emma F. Mkpurunchi <mkpurunchiemma@gmail.com>
2024-07-22 19:00:59 +0100

    'committed'

---
+++ src/app/login/login.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Login = () => {
+    return (
+        <div>
+            <h1>Welcome to DevLinks</h1>
+            <h4>Login</h4>
+        </div>
+    )
+}
+
+export default Login
