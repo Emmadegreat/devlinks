69afc9b6f0d346a9408f115f33a069d84d8a79b1
GewinCodeX <chibuzornworie85@gmail.com>
2024-07-23 15:27:31 +0100

    fix: removed unused code

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,10 @@
 import Image from "next/image";
 import Login from "./login/page";
-import DdesktopAddLinks from "@/components/desktop-add-links";
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-cente bg-[#fafafa]">
-      {/* <Login/> */}
-      <DdesktopAddLinks />
+    <main className="flex flex-col items-center bg-[#fafafa]">
+      <Login/>
     </main>
   );
 }
