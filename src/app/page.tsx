3e6a0bdc99bf1daaae3587afcbaed1c7930920f6
Xcoder2023 <Xcoder2023@gmail.com>
2024-07-24 06:57:57 -0800

    profiling profile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,13 @@
 import Image from "next/image";
-import Login from "./login/page";
-// import ProfileDetails from "@/components/ProfileDetails";
+// import Login from "./login/page";
+import ProfileDetails from "@/components/ProfileDetails";
 
 
 export default function Home() {
   return (
-    <main className="flex flex-col items-center bg-[#fafafa]">
-      <Login/>
+    <main className="flex flex-col items-cente bg-[#fafafa]">
+      {/* <Login/> */}
+			<ProfileDetails/>
     </main>
   );
 }
